9a8647e7fd339cb81235c305a486a690b34861bc
driss21 <drissnfifi41@gmail.com>
2021-03-22 11:04:16 +0100

    fix problem firstName

--- src/main/scala/fr/uge/ia/LabelBase.scala
+++ src/main/scala/fr/uge/ia/LabelBase.scala
@@ -2,6 +2,6 @@ package fr.uge.ia
 
 
 object LabelBase {
-  val INPUT = "file:D:\\programation\\Upem M2\\IA\\Master-IA-Project\\src\\main\\resources\\lubm1.ttl"
+  val INPUT = "file:D:\\5A\\1er semestre\\IA\\Master-IA-Project\\src\\main\\resources\\lubm1.ttl"
 }
 
